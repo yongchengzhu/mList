2174fe04f7f30e9b81f302711c6efc8e3a4cdd6f
yongchengzhu <yongchengzhu002@gmail.com>
2021-01-13 21:40:27 -0500

    copy pasta table sort.

--- src/main/js/client/src/components/BookShelfPage/BookTable/BookTable.tsx
+++ src/main/js/client/src/components/BookShelfPage/BookTable/BookTable.tsx
@@ -11,7 +11,7 @@ import { bookContextUpdateAction } from '../../../redux/actions/book/context';
 const BookTable: FC<{}> = () => {
   const dispatch = useDispatch();
   const { fetchingAll, books } = useSelector((state: RootState) => state.book);
-  console.log('books', books);
+
   const renderTableBody = () => {
     if (fetchingAll) return <LoadingSpinner />;
     return books.map((book) => {
@@ -36,16 +36,73 @@ const BookTable: FC<{}> = () => {
     });
   };
 
+  // copy-pasta from w3school ('ω^＼)
+  function sortTable(n: number) {
+    let table: any, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("book-table");
+    if (!table) return;
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc"; 
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+      //start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /*Loop through all table rows (except the
+      first, which contains table headers):*/
+      for (i = 1; i < (rows.length - 1); i++) {
+        //start by saying there should be no switching:
+        shouldSwitch = false;
+        /*Get the two elements you want to compare,
+        one from current row and one from the next:*/
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /*check if the two rows should switch place,
+        based on the direction, asc or desc:*/
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark that a switch has been done:*/
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        //Each time a switch is done, increase this count by 1:
+        switchcount ++;      
+      } else {
+        /*If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again.*/
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
+
   return (
     <div>
-      <table>
+      <table id="book-table">
         <thead>
           <tr>
-            <th>Title</th>
-            <th>Last Read</th>
-            <th>Rating</th>
-            <th>Read Since</th>
-            <th>Days Left</th>
+            <th onClick={() => sortTable(0)}>Title</th>
+            <th onClick={() => sortTable(1)}>Last Read</th>
+            <th onClick={() => sortTable(2)}>Rating</th>
+            <th onClick={() => sortTable(3)}>Read Since</th>
+            <th onClick={() => sortTable(4)}>Days Left</th>
           </tr>
         </thead>
         <tbody>{renderTableBody()}</tbody>

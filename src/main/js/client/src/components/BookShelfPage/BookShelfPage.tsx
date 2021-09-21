ad6d5e003bbe92c9b697631def8913d179e2cf68
yongchengzhu <yongchengzhu002@gmail.com>
2021-09-20 20:40:13 -0400

    clean.

--- src/main/js/client/src/components/BookShelfPage/BookShelfPage.tsx
+++ src/main/js/client/src/components/BookShelfPage/BookShelfPage.tsx
@@ -16,28 +16,8 @@ const prepareBrokerURL = (path: string): string => {
 
 const BookShelfPage: FC<{}> = () => {
   const dispatch = useDispatch();
-  // let client = null;
-  // const { books } = useSelector(
-  //   (state: RootState) => state.book
-  // );
 
   useEffect(() => {
-    // const client = new Client({ brokerURL: prepareBrokerURL('/chat') });
-
-    // client.activate();
-
-    // client.onConnect = () => {
-    //   console.log('Connected to Web Socket ;)');
-
-    //   client.subscribe('/user/queue/messages', (message) => {
-    //     console.log(`Message received: ${message.body}`);
-    //   });
-    //   console.log("books", books);
-    //   client.publish({
-    //     destination: '/app/chat',
-    //     body: JSON.stringify({ from: 'meow', text: [{ title: "adsf", chapter: "123" }] }),
-    //   });
-    // };
     dispatch(booksFetchActionCreator());
   }, [dispatch]);
 

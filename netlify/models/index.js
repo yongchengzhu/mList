09dea60dde5142c3463b411b7bbbd6c6ee641b83
yongchengzhu <yongchengzhu002@gmail.com>
2022-01-17 09:01:54 -0500

    turn ssl off.

--- netlify/models/index.js
+++ netlify/models/index.js
@@ -2,7 +2,13 @@ const { Sequelize } = require('sequelize');
 require('../configs');
 require('pg');
 
-const sequelize = new Sequelize(process.env.DATABASE_URI);
+const sequelize = new Sequelize(process.env.DATABASE_URI, { dialectOptions: { 
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    } 
+  } 
+});
 
 const User = require('./User')(sequelize);
 const Book = require('./Book')(sequelize);
